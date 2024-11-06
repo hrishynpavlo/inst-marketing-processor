82ad79f28e65e1f4746213fdd3c2b04473634a7f
pavlo.hrishyn <pavlo.hrishyn.dev@gmail.com>
2024-11-07 00:44:51 +0100

    added terraform and clound function

---
+++ infra/main.tf
@@ -0,0 +1,45 @@
+﻿##https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloudbuildv2_connection_iam
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "inst-marketing-processor-proj"
+  region = "europe-west1"
+}
+resource "google_cloudbuild_trigger" "github_analyzer_trigger" {
+  name = "cloud-build-trigger-for-analyzer"
+  
+  github {
+    
+    owner = ""
+    name = ""
+    push {
+      branch = "develop"
+    }
+  }
+  
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = [
+        "builds", "submit", "--tag", "gcr.io/$PROJECT_ID/analyzer-cloud-function", "."
+      ]
+    }
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = [
+        "run", "deploy", "analyzer-cloud-function", "--image", "gcr.io/$PROJECT_ID/analyzer-cloud-function",
+        "--platform", "managed", "--region", "europe-west1", "--allow-unauthenticated"
+      ]
+    }
+    images = ["gcr.io/$PROJECT_ID/analyzer-cloud-function"]
+  }
+}
+
--- src/InstMarketingProcessor.Analyzer/Program.cs
+++ src/InstMarketingProcessor.Analyzer/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace InstMarketingProcessor.Analyzer;
+﻿using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+
+namespace InstMarketingProcessor.Analyzer;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var f = new Function();
+        var ctx = new DefaultHttpContext();
+        await f.HandleAsync(ctx);
+    }
+}
+
+public class Function : IHttpFunction
+{
+    public async Task HandleAsync(HttpContext context)
+    {
+        await context.Response.WriteAsync("{ \"test\": \"test\" }", context.RequestAborted);
     }
 }
