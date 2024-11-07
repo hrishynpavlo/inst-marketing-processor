dfccbde33a0ab1f7a22c9c06895f9af9cbb11bf1
pavlo.hrishyn <pavlo.hrishyn.dev@gmail.com>
2024-11-07 18:20:29 +0100

    added bucket definition

---
+++ infra/modules/bucket/main.tf
@@ -0,0 +1,14 @@
+﻿resource "google_storage_bucket" "statistic_bucket" {
+  name = "inst-statistic"
+  location = "EU"
+  force_destroy = true
+  
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
