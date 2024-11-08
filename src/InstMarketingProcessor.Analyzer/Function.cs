b6e363c70a9f0a145de3d0521d45920764dcc03a
pavlo.hrishyn <pavlo.hrishyn.dev@gmail.com>
2024-11-08 01:06:36 +0100

    test update

--- src/InstMarketingProcessor.Analyzer/Function.cs
+++ src/InstMarketingProcessor.Analyzer/Function.cs
@@ -8,7 +8,7 @@ public class Function : IHttpFunction
 {
     public async Task HandleAsync(HttpContext context)
     {
-        var result = new Response("John Doe", DateTime.UtcNow, Random.Shared.Next(1, 3002));
+        var result = new Response("John Doe", DateTime.UtcNow, Random.Shared.Next(3033, 3999));
         var response = JsonConvert.SerializeObject(result);
         await Ok(context, response);
     }
